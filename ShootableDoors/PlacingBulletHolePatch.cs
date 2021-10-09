b66981221718271a92808f93b57d5a94a36afb6e
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-10-09 16:21:58 +0200

    11.0 update

--- ShootableDoors/PlacingBulletHolePatch.cs
+++ ShootableDoors/PlacingBulletHolePatch.cs
@@ -29,10 +29,10 @@ namespace Mistaken.ShootableDoors
             if (!DoorHandler.DoorsPenetration.TryGetValue(door, out var penetration))
                 return true;
 
-            float damage = (__instance.BaseStats.DamageAtDistance(__instance.Firearm, hit.distance) / (__instance is BuckshotHitreg buckshot ? buckshot._buckshotSettings.MaxHits : 1f)) * 0.1f;
+            float damage = (__instance.Firearm.BaseStats.DamageAtDistance(__instance.Firearm, hit.distance) / (__instance is BuckshotHitreg buckshot ? buckshot._buckshotSettings.MaxHits : 1f)) * 0.1f;
             damage = InventorySystem.Items.Armor.BodyArmorUtils.ProcessDamage(penetration, damage, Mathf.RoundToInt(__instance.Firearm.ArmorPenetration * 100f));
             door.DamageDoor(damage, DoorDamageType.Weapon);
-            __instance.Conn.Send(new InventorySystem.Items.Firearms.BasicMessages.RequestMessage(0, InventorySystem.Items.Firearms.BasicMessages.RequestType.Hitmarker), 0);
+            Hitmarker.SendHitmarker(__instance.Conn, 1f);
             return true;
         }
     }
